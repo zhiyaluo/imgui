dbc6afb18004cb36bc73a9f5ade1adb3043be90c
omar <omarcornut@gmail.com>
2019-04-10 15:24:49 +0200

    RangeSelect/MultiSelect: Fix so that shift+arrow when landing on an item that doesn't hold multi-select data doesn't trigger an unbounded range-select that ends up selecting everything until the end.

--- imgui_internal.h
+++ imgui_internal.h
@@ -823,7 +823,7 @@ struct ImGuiNextItemData
     bool                        OpenVal;        // Set by SetNextItemOpen() function.
     ImGuiCond                   OpenCond;
     void*                       MultiSelectData;
-    bool                        MultiSelectDataIsSet;
+    ImGuiID                     MultiSelectScopeId;
 
     ImGuiNextItemData()         { memset(this, 0, sizeof(*this)); }
     inline void ClearFlags()    { Flags = ImGuiNextItemDataFlags_None; }
